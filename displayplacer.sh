099bdc6f705221ad8d8945c8d88ae3be3a2dcec3
Louie Lee <louie.lee@datax.io>
2025-03-24 10:56:44 +0800

    feat: update displacer script

--- displayplacer.sh
+++ displayplacer.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 # Store the display arrangements in variables
-DISPLAY_ARRANGEMENT_1="displayplacer \"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1800x1169 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0\" \"id:F942DB80-D7EF-4419-80AD-EF6CAF28F036 res:1200x1920 hz:120 color_depth:8 enabled:true scaling:off origin:(1800,-72) degree:270\""
+DISPLAY_ARRANGEMENT_1="displayplacer \"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1710x1112 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0\" \"id:8AD6921A-059F-4684-A2E5-486F4A579B8F res:1920x1200 hz:60 color_depth:8 enabled:true scaling:off origin:(823,-1200) degree:0\""
+DISPLAY_ARRANGEMENT_2="displayplacer \"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1710x1112 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0\" \"id:8AD6921A-059F-4684-A2E5-486F4A579B8F res:1920x1200 hz:60 color_depth:8 enabled:true scaling:off origin:(-113,-1200) degree:0\""
+DISPLAY_ARRANGEMENT_3="displayplacer \"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1710x1112 hz:60 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0\" \"id:29FFC71B-3134-460E-87C0-3E508B0B7F39 res:2560x1440 hz:30 color_depth:8 enabled:true scaling:on origin:(-2560,-645) degree:0\""
 
 DISPLAY_ARRANGEMENT_2="displayplacer \"id:37D8832A-2D66-02CA-B9F7-8F30A301B230 res:1800x1169 hz:120 color_depth:8 enabled:true scaling:on origin:(0,0) degree:0\" \"id:F942DB80-D7EF-4419-80AD-EF6CAF28F036 res:1920x1200 hz:120 color_depth:8 enabled:true scaling:off origin:(-64,-1200) degree:0\""
 
 # ASCII art for arrangements
 ARRANGEMENT_1_ART="
-[1] ┌───────┐┌──┐
-    │   M   ││S1│
-    └───────┘│  │
-             └──┘
-"
-
-ARRANGEMENT_2_ART="
-[2] ┌──────┐
-    │  S1  │
-    └──────┘
+[1]     ┌──────┐
+        │  S1  │
+        └──────┘
      │ M │   
      └───┘
+"
+ARRANGEMENT_2_ART="
+[2]     ┌──────┐
+        │  S1  │
+        └──────┘
+         │ M │   
+         └───┘
 "
 
 # Function to show usage
@@ -69,6 +71,10 @@ case $ARRANGEMENT_ID in
     2)
         eval "$DISPLAY_ARRANGEMENT_2"
         echo "$ARRANGEMENT_2_ART"
+        ;;
+    3)
+        eval "$DISPLAY_ARRANGEMENT_3"
+        echo "Office setup without stand"
         ;;
     *)
         echo "Error: Invalid arrangement ID. Please use 1 or 2."
